b090fd3c0c68439749d65733061d9d681562a5b0
Timhojin <ihavealreadylost@gmail.com>
2025-05-15 09:54:57 +1000

    used pygame group instead of directly instances

--- main.py
+++ main.py
@@ -12,19 +12,24 @@ def main():
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
-
-
         dt = clock.tick(60) / 1000
 
 
