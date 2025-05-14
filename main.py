b6439d2ac5530a34d6f47267057aa7725ae33a39
Timhojin <ihavealreadylost@gmail.com>
2025-05-14 14:05:09 +1000

    Added FPS limit

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -13,6 +16,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
